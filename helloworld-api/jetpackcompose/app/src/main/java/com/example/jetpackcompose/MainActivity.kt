06250d455171cae28ef37b62a51d0eb44316f887
piotrkedra <piotr.k9750@gmail.com>
2022-02-28 21:08:32 +0100

    jetpack compose intro 1/2

---
+++ helloworld-api/jetpackcompose/app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcompose.ui.theme.JetpackcomposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackcomposeTheme {
+                MyApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Column(
+            modifier = Modifier.padding(vertical = 4.dp)
+        ) {
+            for (name in names) {
+                Greeting(name)
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if(expanded.value) 40.dp else 0.dp
+    Surface(
+        color=MaterialTheme.colors.primary,
+        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+    ) {
+
+        Row(
+            modifier = Modifier.padding(24.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello ")
+                Text(text = "$name!")
+            }
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if(expanded.value) "Show less" else "Show more")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackcomposeTheme {
+        MyApp()
+    }
+}
