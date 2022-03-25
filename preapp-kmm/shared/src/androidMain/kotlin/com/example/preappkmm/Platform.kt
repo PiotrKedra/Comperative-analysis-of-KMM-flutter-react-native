db63fb153207e6c0149d6b07f5e0af410659f986
piotrkedra <piotr.k9750@gmail.com>
2022-03-25 11:38:32 +0100

    kmm app init

---
+++ preapp-kmm/shared/src/androidMain/kotlin/com/example/preappkmm/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.preappkmm
+
+actual class Platform actual constructor() {
+    actual val platform: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
---
+++ preapp-kmm/shared/src/commonMain/kotlin/com/example/preappkmm/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.preappkmm
+
+expect class Platform() {
+    val platform: String
+}
