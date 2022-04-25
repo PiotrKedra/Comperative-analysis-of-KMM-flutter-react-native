b008878e4a3cae4a0cf1bb6df7ac2c967916b58e
piotrkedra <piotr.k9750@gmail.com>
2022-04-25 22:27:34 +0200

    search bar

--- preapp-kmm/androidPreeappKMM/src/main/java/com/example/preappkmm/android/presentation/recipe_list/RecipeListScreen.kt
+++ preapp-kmm/androidPreeappKMM/src/main/java/com/example/preappkmm/android/presentation/recipe_list/RecipeListScreen.kt
@@ -1,9 +1,11 @@
 package com.example.preappkmm.android.presentation.recipe_list
 
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import com.example.preappkmm.android.presentation.recipe_list.components.RecipeList
+import com.example.preappkmm.android.presentation.recipe_list.components.SearchAppBar
 import com.example.preappkmm.android.presentation.theme.AppTheme
 import com.example.preappkmm.interactors.recipe_list.RecipeListEvents
 import com.example.preappkmm.presentation.recipe_list.RecipeListState
@@ -17,14 +19,26 @@ fun RecipeListScreen(
     onClickRecipeListItem: (Int) -> Unit,
 ){
     AppTheme(displayProgressBar = state.isLoading) {
-        RecipeList(
-            loading = state.isLoading,
-            recipes = state.recipes,
-            page = state.page,
-            onTriggerNextPage = {
-                onTriggerEvent(RecipeListEvents.NextPage)
+
+        Scaffold(
+            topBar = {
+                SearchAppBar(
+                    query = state.query,
+                    onQueryChange = {},
+                    onExecuteSearch = {}
+                )
             },
-            onClickRecipeListItem = onClickRecipeListItem
-        )
+        ) {
+            println(it)
+            RecipeList(
+                loading = state.isLoading,
+                recipes = state.recipes,
+                page = state.page,
+                onTriggerNextPage = {
+                    onTriggerEvent(RecipeListEvents.NextPage)
+                },
+                onClickRecipeListItem = onClickRecipeListItem
+            )
+        }
     }
 }
