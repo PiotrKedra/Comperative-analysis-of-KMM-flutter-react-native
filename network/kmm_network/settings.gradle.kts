d160ebb444875600d6c0845ab01706be2df2b247
piotrkedra <piotr.k9750@gmail.com>
2022-05-05 18:41:45 +0200

    init kmm network project

---
+++ network/kmm_network/settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "kmm_network"
+include(":androidNetworkApp")
+include(":shared")
